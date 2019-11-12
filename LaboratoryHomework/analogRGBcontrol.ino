67e75e27fa4b0e19bd8b33bbb224eb022b435b4a
bajanioana <bajanioanagabi@yahoo.com>
2019-11-13 00:19:06 +0200

    analogRGBcontrol.ino

--- LaboratoryHomework/analogRGBcontrol.ino
+++ LaboratoryHomework/analogRGBcontrol.ino
@@ -1,23 +1,23 @@
-const int potPin1 = A0;
-const int potPin2 = A1;
-const int potPin3 = A2; 
+const int potRed = A0;
+const int potGreen = A1;
+const int potBlue = A2; 
 
 const int redPin = 9;
 const int greenPin = 10;
 const int bluePin = 11;
 
-int potValue1 = 0;
-int potValue2 = 0;
-int potValue3 = 0;
+int potValueRed = 0;
+int potValueGreen = 0;
+int potValueBlue = 0;
 
 int redValue = 0;
 int greenValue = 0;
 int blueValue = 0;
 
 void setup() {
-  pinMode(potPin1, INPUT);
-  pinMode(potPin2, INPUT);
-  pinMode(potPin3, INPUT);
+  pinMode(potRed, INPUT);
+  pinMode(potGreen, INPUT);
+  pinMode(potBlue, INPUT);
   pinMode(redPin, OUTPUT);
   pinMode(greenPin, OUTPUT);
   pinMode(bluePin, OUTPUT);
@@ -27,21 +27,27 @@ void setup() {
 }
 
 void loop() {
-  potValue1 = analogRead(potPin1);
-  potValue2 = analogRead(potPin2);
-  potValue3 = analogRead(potPin3);
+  potValueRed = analogRead(potRed);
+  potValueGreen = analogRead(potGreen);
+  potValueBlue = analogRead(pot Blue);
 
+  redValue = map(potValueRed, 0, 1023, 0, 255);
+  greenValue = map(potValueGreen, 0, 1023, 0, 255);
+  blueValue = map(potValueBlue, 0, 1023, 0, 255);
 
-  redValue = map(potValue1, 0, 1023, 0, 255);
-  greenValue = map(potValue2, 0, 1023, 0, 255);
-  blueValue = map(potValue3, 0, 1023, 0, 255);
-
-  analogWrite(redPin, redValue);
-  analogWrite(greenPin, greenValue);
-  analogWrite(bluePin, blueValue);
+  setColor(redValue, greenValue, blueValue);
   
   Serial.println(redValue);
   Serial.println(greenValue);
   Serial.println(blueValue);
   delay(1);
+  
+}
+
+void setColor(int red, int green, int blue){
+
+ analogWrite(redPin, red);
+  analogWrite(greenPin, green);
+  analogWrite(bluePin, blue);
+  
 }
